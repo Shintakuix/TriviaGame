81f8e60149b5c474da5d025b9e938ed44e1acf82
shintakuix <candeiasva@gmail.com>
2019-06-21 11:48:30 -0400

    Final Code

--- assest/javascript/game.js
+++ assest/javascript/game.js
@@ -1,17 +1,55 @@
-var questions = [{q: "this is question 1", a: "this is answer 1"} , {q: "this is question 2", a:"this is answer 2"}];
-console.log(questions[0].q);
-console.log(questions[1].q);
 var rightAnswer = 0;
 var wrongAnswer = 0;
+var leftblank = 0;
 var playerAnswers = [];
 var right = false;
+var number = 60;
+var intervalId;
 
 
+    $("#question1").hide();
+    $("#question2").hide();
+    $("#question3").hide();
+    $("#question4").hide();
+    $("#finish").hide();
+    $("#cor").hide();
+    $("#inc").hide();
+    $("#lef").hide();
+    $("#restart").hide();
 
-$("#first-question").text(questions[0].q);
-$("#second-question").text(questions[1].q);
+$("#start").on("click", run);
+
+function run() {
+    $("#start").hide();
+    $("#show-number").html("<h2>60</h2>");
+    $("#question1").show();
+    $("#question2").show();
+    $("#question3").show();
+    $("#question4").show();
+    $("#finish").show();
+   
+clearInterval(intervalId);
+intervalId = setInterval(decrement, 1000);
+
+
+function decrement() {
+
+    number--;
+
+    $("#show-number").html("<h2>" + number + "</h2>");
+
+    if (number === 0) {
+
+    clearInterval(intervalId);
+    rungame();    
+    alert("Time Up!");
+    $("#show-number").html("timeup");
+    }
+}
+}
 
 $("#finish").on("click",rungame);
+clearInterval(intervalId);
 
 function compareAnswer1() {
     answerXD = document.getElementById("question1").getElementsByTagName("input");
@@ -26,6 +64,9 @@ function compareAnswer1() {
             playerAnswers.push(answerXD[i].value);
             console.log(playerAnswers);
         }
+       /*  else {
+            playerAnswers[0] = "wrong";
+        } */
     }
 }
 function compareAnswer2() {
@@ -41,6 +82,9 @@ function compareAnswer2() {
             playerAnswers.push(answerXD[i].value);
             console.log(playerAnswers);
         }
+  /*       else {
+            playerAnswers[1] = "wrong";
+        } */
     }
 }
 
@@ -57,6 +101,9 @@ function compareAnswer3() {
             playerAnswers.push(answerXD[i].value);
             console.log(playerAnswers);
         }
+/*         else {
+            playerAnswers[2] = "wrong";
+        } */
     }
 }
 
@@ -73,14 +120,33 @@ function compareAnswer4() {
             playerAnswers.push(answerXD[i].value);
             console.log(playerAnswers);
         }
+/*         else {
+            playerAnswers[3] = "wrong";
+        } */
     }
 }
 
 function rungame() {
+
+
+    $("#start").hide();
+    $("#show-number").hide();
+    $("#question1").hide();
+    $("#question2").hide();
+    $("#question3").hide();
+    $("#question4").hide();
+    $("#finish").hide();
+    $("#restart").show();
+    $("#inc").show();
+    $("#cor").show();
+    $("#lef").show();
+
     compareAnswer1();
     compareAnswer2();
     compareAnswer3();
     compareAnswer4();
+
+    console.log(playerAnswers);
     if (playerAnswers[0] == "right") {
         rightAnswer++;
     }
@@ -93,27 +159,31 @@ function rungame() {
     if (playerAnswers[1] == "wrong") {
         wrongAnswer++;
     }
-document.getElementById("corrects").innerHTML = rightAnswer;
-document.getElementById("incorrects").innerHTML = wrongAnswer;
-    
-}
-    /*     if(right) {
+    if (playerAnswers[2] == "right") {
         rightAnswer++;
     }
-    else {
+    if (playerAnswers[2] == "wrong") {
         wrongAnswer++;
-    } */
-        /* if (answerXD == questions[0].q || questions[1].q) {
-            rightAnswer++;
-        }
-        if (answerXD !== questions[0].q || questions[1].q) {
-            wrongAnswer++;
-        } */
+    }
+    if (playerAnswers[3] == "right") {
+        rightAnswer++;
+    }
+    if (playerAnswers[3] == "wrong") {
+        wrongAnswer++;
+    }
+document.getElementById("corrects").innerHTML = rightAnswer;
+document.getElementById("incorrects").innerHTML = wrongAnswer;
+document.getElementById("leftblank").innerHTML = 4 - (rightAnswer + wrongAnswer);
+    rightAnswer = 0;
+    wrongAnswer = 0;
+    playerAnswers = [];    
+}
 
+$("#restart").on("click",restart);
 
-/* $( "input" ).on( "click", function() {
-    $( "#log" ).html( $( "input:checked" ).val() + " is checked!" );
-  }); */
+function restart() {
+    location.reload();
+}
 
 
 
